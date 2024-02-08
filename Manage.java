ec109803285547688713aac6c1f68196cc1c31f5
Irish Mancera <93238175+BILLY-RED@users.noreply.github.com>
2024-02-07 18:29:23 -1200

    Add files via upload

---
+++ Manage.java
@@ -0,0 +1,35 @@
+
+//IrishMancera
+//BSIT3A,2nd SEMESTER
+//08-02-2024
+//IT106-Application Development
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Manage {
+
+    public static void Manage(String filename, String email) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filename, true))) {
+            writer.println(email); // Append email to file
+            System.out.println("Email appended to file successfully.");
+        } catch (IOException e) {
+            System.err.println("Error: Unable to open file.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String filename = "account.txt"; // Name of the file to append emails
+        String email;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the email to append: ");
+        email = scanner.nextLine();
+
+        Manage(filename, email);
+
+        scanner.close();
+    }
+}
